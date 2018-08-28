8c46e4cc38511343df92d622042faac71f958980
Rahul Sahay <rahul.sahay@voya.com>
2018-08-28 12:43:46 -0400

    remove mongodb

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),
+    //mongoose = require('mongoose'),
     Task = require('./api/models/testModel'); //created model loading here
     //bodyParser = require('body-parser');
 
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Tododb');
+//mongoose.Promise = global.Promise;
 
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+//mongoose.connect('mongodb+srv://application:application@cluster0-zht8z.mongodb.net/test?retryWrites=true');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 var routes = require('./api/routes/testRoutes'); //importing route
 routes(app); //register the route
