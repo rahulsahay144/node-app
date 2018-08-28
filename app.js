b780f28b16a25ce2714605e29777063253a91d9a
Rahul Sahay <rahul.sahay@voya.com>
2018-08-28 00:37:52 -0400

    fix

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/testModel'), //created model loading here
+    bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/testRoutes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
